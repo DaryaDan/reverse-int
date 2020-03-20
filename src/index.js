bfb31bfa46d507cf2c8fc87c1eb4d9ba36767a64
DaryaDan <56804467+DaryaDan@users.noreply.github.com>
2020-03-20 16:28:48 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ module.exports = function reverse (n) {
 var a;
 if(n<0){
 a = ('' + n).slice(1);
-return (('' + q).split('').reverse().join(''));
+return (('' + a).split('').reverse().join(''));
 }
 return (('' + n).split('').reverse().join(''));
 }
